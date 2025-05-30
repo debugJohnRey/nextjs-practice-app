924b7719adb8bf610791f47e6e5d05b4e5d1475b
johnreysoriano46@gmail.com <136785251+debugJohnRey@users.noreply.github.com>
2025-05-30 21:19:10 +0800

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Geist } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin']
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin']
+});
+
+export const geist = Geist({
+  subsets: ['latin']
+});
